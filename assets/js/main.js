623eb7e8124751f34cfbadb7adf734f3c68df3e1
Sumit Grover <sumitgrover108@gmail.com>
2024-08-17 18:30:25 +0530

    added registration popup and updated section

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,6 +74,26 @@
       behavior: 'smooth'
     })
   }
+  
+  /**
+   * Registration popup logic
+   */
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const popup = document.getElementById("popup");
+    const closeBtn = document.querySelector(".close-btn");
+    popup.style.display = "flex";
+
+    closeBtn.addEventListener("click", function() {
+        popup.style.display = "none";
+    });
+    window.addEventListener("click", function(event) {
+        if (event.target === popup) {
+            popup.style.display = "none";
+        }
+    });
+});
+
 
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
